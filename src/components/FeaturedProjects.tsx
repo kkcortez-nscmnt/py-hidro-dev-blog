ded81ab30f4a6c3c5d934b4bbb58c85081265971
Jamilson do Nascimento <jamil.pyhidrodev@gmail.com>
2025-08-06 18:56:48 -0300

    comp: implementa project card

---
+++ src/components/FeaturedProjects.tsx
@@ -0,0 +1,10 @@
+import type { Project } from "@/types/types"
+
+
+const FeaturedProjects = () => {
+  return (
+    <div>FeaturedProjects</div>
+  )
+}
+
+export default FeaturedProjects
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,32 @@
+import type { Project } from "@/types/types";
+import Link from "next/link";
+import Image from "next/image";
+
+const ProjectCard = ({ project }: { project: Project }) => {
+  return (
+    <Link
+      href={`/projects/${project.documentId}`}
+      className="block transform transition duration-300 hover:scale-[1.02]"
+    >
+      <div className="bg-gray-800 border border-gray-700 rounded-lg overflow-hidden shadow-md transition hover:shadow-md">
+        <Image
+          src={project.image}
+          alt={project.title}
+          className="w-full h-40 object-cover"
+        />
+        <div className="p-5">
+          <h3 className="text-3xl font-semibold text-blue-400 mb-1">
+            {project.title}
+          </h3>
+          <p className="text-sm text-gray-300 mb-2">{project.description}</p>
+          <div className="flex justify-between items-center text-sm text-gray-400">
+            <span>{project.category}</span>
+            <span>{new Date(project.date).toLocaleDateString()}</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProjectCard;
